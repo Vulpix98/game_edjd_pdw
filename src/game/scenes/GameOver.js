cdd851d10e21cb47c9ba93cabd2b48d30bbf3b24
Vulpix98 <leonelqo@hotmail.com>
2025-01-10 10:58:17 +0000

    gameover

--- src/game/scenes/GameOver.js
+++ src/game/scenes/GameOver.js
@@ -9,6 +9,10 @@ export class GameOver extends Phaser.Scene {
     }
 
     create() {
+        this.add.text(this.cameras.main.centerX, this.cameras.main.centerY, 'Game Over', {
+            fontSize: '64px',
+            fill: '#ffffff'
+        }).setOrigin(0.5, 0.5);
     }
 }
 
